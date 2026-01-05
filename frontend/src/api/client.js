0719f2006aa9ab0ac3c132fac6f64ba86ce380d6
hello-world123pratik <pratikshirsath45@gmail.com>
2026-01-05 22:51:54 +0530

    Change createImportJob to stringify request body
    Updated createImportJob to send JSON stringified data.

--- frontend/src/api/client.js
+++ frontend/src/api/client.js
@@ -2,6 +2,9 @@ import axios from "axios";
 
 const API = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
 
 export const fetchHome = () => API.get("/");
@@ -9,7 +12,7 @@ export const fetchHome = () => API.get("/");
 export const fetchImages = () => API.get("/images");
 
 export const createImportJob = (url) =>
-  API.post("/import/google-drive", { url });
+  API.post("/import/google-drive", JSON.stringify({ url }));
 
 export const fetchJobStatus = (jobId) =>
   API.get(`/jobs/${jobId}`);
